9ed24a1203dd5664b7025ad979c978e65659da37
Matleyx78 <matmat78@hotmail.it>
2018-10-25 10:20:57 +0200

    Create graph.php

---
+++ views/test/graph.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+//$this->load->view('templates/header');
+
+//$arr = get_defined_vars();
+//echo '<pre>';
+//print_r($_ci_vars);
+//echo '</pre>';
+
+?>
+
+<iframe src="<?php echo site_url('prova/get_svg'); ?>" type="image/svg+xml" width="600" height="400"></iframe>
+<?php //$this->load->view('templates/footer');?>
